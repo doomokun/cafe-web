92963b21f4032408436d71cd812fbe180fb9cb8e
tommyleung <lck.lck01@gmail.com>
2022-10-22 11:58:43 +0800

    rollback/ axios base url

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import axios from 'axios'
 
 import './assets/main.css'
 
 const app = createApp(App)
-
-axios.defaults.baseURL = import.meta.env.VITE_API_ROOT
 
 app.use(router)
 
